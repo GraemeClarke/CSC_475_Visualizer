848de95a9c56d52ab317e4bf6c434a22a8600a99
GraemeClarke <graeme.d.clarke@gmail.com>
2017-04-07 14:33:23 -0700

    change between scenes with 1 2 and 3 keys

---
+++ Assets/sceneModify.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneModify : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("1")) {
+			Application.LoadLevel(0);
+		}
+		else if (Input.GetKeyDown ("2")) {
+			Application.LoadLevel(1);
+		}
+		else if (Input.GetKeyDown ("3")) {
+			Application.LoadLevel(2);
+		}
+		
+	}
+}
