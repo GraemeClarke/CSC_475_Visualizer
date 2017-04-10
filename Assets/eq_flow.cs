dd88ccd56bcd7f592cf80f5951f3ea6b1dc1e3a5
Spencer Vatrt-Watts <asvw@uvic.ca>
2017-04-09 17:18:46 -0700

    Add flow visualizer

--- Assets/eq_flow.cs
+++ Assets/eq_flow.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class eq_flow : MonoBehaviour {
 
 	private GameObject prefab;
-	private GameObject prefab2;
 
-	public enum DrawMode {Circle, Line};
-	public DrawMode drawmode; 
 	public int numberOfObjects;
-	public float radius;
 
 	GameObject [] cubes;
-	GameObject [] cubes2;
-
-	Color altColor = Color.white;
 
 
 	// Initialize scene
@@ -25,32 +18,23 @@ public class eq_flow : MonoBehaviour {
 		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
 
 		prefab = Resources.Load("objects/Cube") as GameObject;
-		prefab2 = Resources.Load ("objects/Cube 2") as GameObject;
 
 		Renderer rend = GetComponent<Renderer>();
 
-		float lineLength = 20;
-		float intialLinePos = 0 - lineLength / 2;
+//		float lineLength = 20;
+//		float intialLinePos = 0 - lineLength / 2;
 
-		for (int i = 0; i < numberOfObjects; i++) {
-			
-			float angle = i * Mathf.PI * 2 / numberOfObjects;
-			Vector3 circPos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
-			Vector3 linePos = new Vector3(intialLinePos + i*lineLength/numberOfObjects, 0, 0);
-
-			if (drawmode == DrawMode.Circle) {
-				Instantiate(prefab, circPos, Quaternion.identity);
-				Instantiate(prefab2, circPos, Quaternion.identity);
-				cam.transform.position += new Vector3(0,1/(float)numberOfObjects,-0.5f/(float)numberOfObjects);
-
-			} else if (drawmode == DrawMode.Line) {
-				Instantiate(prefab, linePos, Quaternion.identity);
-				Instantiate(prefab2, linePos, Quaternion.identity);
-			}
-
-		}
-		cubes = GameObject.FindGameObjectsWithTag ("cubes");
-		cubes2 = GameObject.FindGameObjectsWithTag("cubes2");
+//		for (int i = 0; i < numberOfObjects; i++) {
+//			
+//			Vector3 linePos = new Vector3(intialLinePos + i*lineLength/numberOfObjects, 0, 0);
+//
+//			Instantiate(prefab, linePos, Quaternion.identity);
+//		}
+//		cubes = GameObject.FindGameObjectsWithTag ("cubes");
+//
+//		for (int i = 0; i < cubes.Length; i++) {
+//			cubes.Add (cubes [i]);
+//		}
 	}
 	
 	// Update scene (called once per frame)
@@ -64,10 +48,24 @@ public class eq_flow : MonoBehaviour {
 
 		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
 
+		float lineLength = 20;
+		float intialLinePos = 0 - lineLength / 2;
+
+		cubes = GameObject.FindGameObjectsWithTag ("cubes");
+
+		for (int i = 0; i < cubes.Length; i++) {
+
+			if (cubes [i].transform.position.z < 10) {
+				cubes [i].transform.Translate(Vector3.forward * Time.deltaTime * 4);
+			} else {
+				Destroy (cubes [i]);
+
+			}
+
+
+		}
+
 		for (int i = 0; i < numberOfObjects; i++) {
-			
-			Vector3 scale = cubes [i].transform.localScale;
-			Vector3 cubePos = cubes[i].transform.position;
 
 			float spectrum_height = spectrum[i] * 40;
 			int spectrum_max = 5;
@@ -76,22 +74,13 @@ public class eq_flow : MonoBehaviour {
 				spectrum_height = spectrum_height* 0.5f;
 			}
 
-			scale.y = Mathf.Lerp(scale.y, spectrum_height, Time.deltaTime * 30);
+			Vector3 linePos = new Vector3(intialLinePos + i*lineLength/numberOfObjects, 0, 0);
 
-			Vector3 modPos = new Vector3(cubes[i].transform.position.x,scale.y/2,cubes[i].transform.position.z);
-			Vector3 modPos2 = new Vector3(cubes[i].transform.position.x,spectrum_height,cubes[i].transform.position.z);
+			prefab.transform.localScale = new Vector3 (prefab.transform.localScale.x, spectrum_height, prefab.transform.localScale.z);
+			linePos += new Vector3 (0, spectrum_height/2, 0);
 
-			cubes [i].GetComponent<Renderer> ().material.color = altColor;
-			cubes [i].transform.position = modPos;
-			cubes [i].transform.localScale = scale;
-
-			if (spectrum_height > cubes2 [i].transform.position.y) {
-				cubes2[i].transform.position = modPos2;
-			}
+			Instantiate(prefab, linePos, Quaternion.identity);
 		}
 
-		if (drawmode == DrawMode.Circle) {
-			cam.transform.RotateAround (Vector3.zero, new Vector3(0,1,0), 5*Time.deltaTime);
-		}
 	}
 }
